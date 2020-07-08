50e161fa37826cf9a5100cdcf424e871aced0656
Shahid N. Shah <shahid@shah.org>
2020-07-08 18:07:24 -0400

    initial commit after extraction from IGS Specification Commons

---
+++ mod.ts
@@ -0,0 +1,88 @@
+import { Context } from "https://cdn.jsdelivr.net/gh/shah/context-manager/mod.ts";
+import * as inflect from "https://cdn.jsdelivr.net/gh/shah/text-inflect/mod.ts";
+export { snakeCaseValue as name } from "https://cdn.jsdelivr.net/gh/shah/text-inflect/mod.ts";
+
+export interface FutureInterpolatableValue {
+  readonly isFutureInterpolatableValue: true;
+  readonly name: inflect.InflectableValue;
+  prepare(ctx: Context, ...args: any): string;
+}
+
+export function isFutureInterpolatableValue(
+  v: any,
+): v is FutureInterpolatableValue {
+  return (typeof v === "object" && "isFutureInterpolatableValue" in v);
+}
+
+export interface DynamicValue {
+  (ctx: Context, ...args: any): any;
+}
+
+export type Value = DynamicValue | FutureInterpolatableValue | any;
+
+export function resolveValue(
+  ctx: Context,
+  value: Value,
+  ...args: any
+): any {
+  if (isDynamicValue(value)) return value(ctx, ...args);
+  if (isFutureInterpolatableValue(value)) return value.prepare(ctx, ...args);
+  return value;
+}
+
+export function isDynamicValue(v: any): v is DynamicValue {
+  return (typeof v === "function");
+}
+
+export interface DynamicTextValue {
+  (ctx: Context, ...args: any): string;
+}
+
+export type TextValue = string | FutureInterpolatableValue | DynamicTextValue;
+
+export function resolveTextValue(
+  ctx: Context,
+  value: TextValue,
+  ...args: any
+): string {
+  if (typeof value === "string") return value;
+  if (isDynamicTextValue(value)) return value(ctx, ...args);
+  return value.prepare(ctx, ...args);
+}
+
+export function isDynamicTextValue(v: any): v is DynamicTextValue {
+  return (typeof v === "function");
+}
+
+export interface DynamicNumericValue {
+  (ctx: Context, ...args: any): number;
+}
+
+export type NumericValue =
+  | number
+  | FutureInterpolatableValue
+  | DynamicNumericValue;
+
+export function resolveNumericValue(
+  ctx: Context,
+  value: NumericValue,
+  ...args: any
+): number | FutureInterpolatableValue {
+  if (typeof value === "number") return value;
+  if (isDynamicNumericValue(value)) return value(ctx, ...args);
+  return value;
+}
+
+export function resolveNumericValueAsText(
+  ctx: Context,
+  value: NumericValue,
+  ...args: any
+): string {
+  if (typeof value === "number") return value.toString();
+  if (isDynamicNumericValue(value)) return value(ctx, ...args).toString();
+  return value.prepare(ctx, ...args);
+}
+
+export function isDynamicNumericValue(v: any): v is DynamicNumericValue {
+  return (typeof v === "function");
+}
---
+++ mod_test.ts
@@ -0,0 +1,62 @@
+import {
+  Context,
+  ctxFactory,
+  isProjectContext,
+} from "https://cdn.jsdelivr.net/gh/shah/context-manager/mod.ts";
+import {
+  assert,
+  assertEquals,
+} from "https://deno.land/std@v0.60.0/testing/asserts.ts";
+import * as v from "./mod.ts";
+
+const testCtx = ctxFactory.projectContext(".");
+
+Deno.test("Literal and Dynamic Value (variadic)", () => {
+  const strValue: string = "textValue";
+  const numValue: number = 4;
+  const dynValue: v.Value = (ctx: Context, ...extraArgs: any): any => {
+    assert(isProjectContext(ctx));
+    return "value of test model text property(dynamic)" + extraArgs;
+  };
+
+  assert(!v.isDynamicValue(strValue));
+  assert(!v.isDynamicValue(numValue));
+  assert(v.isDynamicValue(dynValue));
+
+  assertEquals(v.resolveTextValue(testCtx, strValue), "textValue");
+  assertEquals(
+    v.resolveValue(testCtx, dynValue, "-append"),
+    "value of test model text property(dynamic)-append",
+  );
+});
+
+Deno.test("Literal and Dynamic TextValue", () => {
+  const strValue: v.TextValue = "textValue";
+  const dynValue: v.TextValue = (ctx: Context, ...extraArgs: any): string => {
+    assert(isProjectContext(ctx));
+    return "value of test model text property(dynamic)" + extraArgs;
+  };
+
+  assert(!v.isDynamicTextValue(strValue));
+  assert(v.isDynamicTextValue(dynValue));
+
+  assertEquals(v.resolveTextValue(testCtx, strValue), "textValue");
+  assertEquals(
+    v.resolveTextValue(testCtx, dynValue, "-append"),
+    "value of test model text property(dynamic)-append",
+  );
+});
+
+Deno.test("Literal and Dynamic NumericValue", () => {
+  const numValue: number = 4;
+  const dynValue: v.NumericValue = (ctx: Context, extraArg: number): number => {
+    assert(isProjectContext(ctx));
+    return 5015 + 4 + extraArg;
+  };
+
+  assert(!v.isDynamicValue(numValue));
+  assert(v.isDynamicValue(dynValue));
+
+  assertEquals(v.resolveNumericValue(testCtx, numValue), 4);
+  assertEquals(v.resolveNumericValue(testCtx, dynValue, 4), 5023);
+});
