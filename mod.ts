c8af0a3cdc5ea1771aa4e3d136c5426ee6e898b3
Shahid N. Shah <shahid@shah.org>
2020-09-15 23:09:27 -0400

    Resolve Deno 1.4 lint errors

--- mod.ts
+++ mod.ts
@@ -1,4 +1,4 @@
-import type { Context } from "https://denopkg.com/shah/context-manager@v1.0.5/mod.ts";
+import type { Context } from "https://denopkg.com/shah/context-manager@v1.0.6/mod.ts";
 import type * as inflect from "https://denopkg.com/shah/text-inflect@v1.0.4/mod.ts";
 export { snakeCaseValue as name } from "https://denopkg.com/shah/text-inflect@v1.0.4/mod.ts";
 
